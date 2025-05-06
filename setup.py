890ed0abdcba52ef5d1b9acf40fbe95cc562d4c4
Frank T. Bergmann <frank.thomas.bergmann@gmail.com>
2025-05-06 10:58:31 +0200

    - add long description

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
 from setuptools import setup
 import versioneer
 
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+    
+# Replace relative image path with absolute GitHub URL
+long_description = long_description.replace(
+    './doc/demo.gif',
+    'https://github.com/copasi/python-petab-importer/raw/master/doc/demo.gif'
+)
+
+
 ENTRY_POINTS = {
     'console_scripts': [
         'copasi_petab_import=copasi_petab_importer.convert_petab:main',
@@ -23,4 +33,6 @@ setup(
     package_data={'copasi_petab_importer': ['*.ui']},
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 )
